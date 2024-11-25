84348b6e2c8e2b7a1f20fb78f410d15d52d456c1
chiman <1164617068@qq.com>
2024-11-25 23:42:54 +0800

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,10 @@
+import foodsStore from "./modules/takeaway"
+import { configureStore } from "@reduxjs/toolkit"
 
+const store = configureStore({
+    reducer: {
+        foods: foodsStore,
+    },
+})
+
+export default store
